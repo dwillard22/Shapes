3c4b00b82644615f0de85723f51cf2d0a12eafdd
dwillard22 <dominicjwillard@gmail.com>
2023-02-24 11:37:44 -0500

    first commit

---
+++ Shapes/Shapes1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes1
+{
+    internal class Class1
+    {
+        private int size;
+
+        public Class1()
+        {
+            size = 8;
+        }
+        public Class1(int s)
+        {
+            SetSize(s);
+        }
+        public void SetSize(int n)
+        {
+            if((n>0) && (n < 40))
+            {
+                size = n;
+            }
+            else
+            {
+                Console.WriteLine("Error wrong size" + n);
+            }
+        }
+        public int GetSize() { return size; }   
+    }
+}
---
+++ Shapes/Shapes1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Shapes1;
+
+static void Main(string[] args)
+{
+    Class1 sh1 = new Class1();
+    Console.Write(sh1.GetSize());
+
+    Class1 sh2 = new Class1(12);
+    Console.Write(sh2.GetSize());
+
+}
